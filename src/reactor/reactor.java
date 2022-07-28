4bbf8d7321613cad1b55d059386b14c9d13c6136
Alfredo Escalante <escalantealf@gmail.com>
2022-07-28 13:55:15 -0400

    Working from alternate machine test.
    Test commit working from alternate machine.

--- src/reactor/reactor.java
+++ src/reactor/reactor.java
@@ -5,6 +5,7 @@ public class reactor {
 	public static void main (String args []) {
 	
 		System.out.println("Testing Git!");
+		System.out.println("Testing now working from 2 different machines! :D");
 		
 	}
 	
